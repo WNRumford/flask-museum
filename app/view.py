ca759c57d47b82296eb50ab20d7adff684b68aed
Kirill <dukennt@gmail.com>
2019-01-15 20:54:33 +0000

    Created main page with fake news

--- app/view.py
+++ app/view.py
@@ -1,7 +1,10 @@
 from app import app
 from flask import render_template, url_for
+from data import news
+from datetime import datetime
 
 @app.route('/')
 def index():
     title = "Главная"
-    return render_template('index.html', t=title)
+    n = news
+    return render_template('index.html', t=title, n=n)
